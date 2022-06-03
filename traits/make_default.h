03b82fb9aea279c2b186dda146ceae095208835f
danakj <danakj@chromium.org>
2022-06-03 11:55:38 -0400

    Add a MakeDefault trait.
    The MakeDefault trait allows construction of a type T if it either
    - is default-constructible
    - or has a static with_default() method that returns type T.
    
    MakeDefault<T>::has_trait reports if T has the trait. A type
    must be either default-constructible or provide with_default()
    but can not be both in order to implement the trait.
    
    MakeDefault<T>::make_default() returns a T constructed with its
    default value, and requires that T is MakeDefault.

---
+++ traits/make_default.h
@@ -0,0 +1,53 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <type_traits>
+
+namespace sus::traits {
+
+namespace __private {
+
+template <class T, class Signature>
+static constexpr bool has_with_default(...) {
+  return false;
+}
+
+template <class T, class R, class... Args>
+  requires(
+      std::is_same_v<decltype(T::with_default(std::declval<Args>()...)), R>)
+static constexpr bool has_with_default(int) {
+  return true;
+}
+
+}  // namespace __private
+
+template <class T>
+struct MakeDefault {
+  static constexpr bool has_trait =
+      std::is_default_constructible_v<T> ^ __private::has_with_default<T, T>(0);
+
+  static constexpr T make_default() noexcept {
+    static_assert(has_trait,
+                  "MakeDefault trait used when the trait is not present for "
+                  "type T. Verify with MakeDefault<T>::has_trait.");
+    if constexpr (std::is_default_constructible_v<T>)
+      return T();
+    else
+      return T::with_default();
+  }
+};
+
+}  // namespace sus::traits
