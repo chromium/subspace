53a4a75c7359f76d43d37ec3b6d600f43c39fa6f
Dana Jansens <danakj@chromium.org>
2022-06-06 16:28:53 -0400

    Add an Array container type
    It just has construction and get()/get_mut() so far.

---
+++ containers/array.h
@@ -0,0 +1,105 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <type_traits>
+
+#include "assertions/check.h"
+#include "concepts/make_default.h"
+#include "marker/unsafe.h"
+
+namespace sus::containers {
+
+template <class T, size_t N>
+class Array;
+
+template <class T, size_t N>
+class Array {
+ public:
+  constexpr static Array with_default()
+    requires(::sus::concepts::MakeDefault<T>::has_trait)
+  {
+    auto a = Array();
+    if constexpr (N > 0) {
+      for (size_t i = 0; i < N; ++i) a.storage_.data_[i] = T();
+    }
+    return a;
+  }
+
+  constexpr static Array with_uninitialized(::sus::marker::UnsafeFnMarker)
+    requires(::std::is_trivially_default_constructible_v<T>)
+  {
+    return Array();
+  }
+
+  template <class InitializerFn>
+  constexpr static Array with_initializer(InitializerFn f) {
+    return Array(kWithInitializer, static_cast<decltype(f)&&>(f),
+                 std::make_index_sequence<N>());
+  }
+
+  constexpr static Array with_value(const T& t)
+    requires(std::is_copy_constructible_v<T>)
+  {
+    return Array(kWithValue, t, std::make_index_sequence<N>());
+  }
+
+  constexpr const T& get(size_t i) const&
+    requires(N > 0)
+  {
+    ::sus::check(i < N);
+    return storage_.data_[i];
+  }
+  constexpr const T& at(size_t i) && = delete;
+
+  T& get_mut(size_t i) &
+        requires(N > 0)
+  {
+    ::sus::check(i < N);
+    return storage_.data_[i];
+  }
+
+ private:
+  Array() {}
+
+  enum WithInitializer { kWithInitializer };
+  template <class F, size_t... Is>
+  constexpr Array(WithInitializer, F&& f, std::index_sequence<Is...>) noexcept
+      : storage_{((void)Is, f())...} {}
+
+  enum WithValue { kWithValue };
+  template <size_t... Is>
+  constexpr Array(WithValue, const T& t, std::index_sequence<Is...>) noexcept
+      : storage_{((void)Is, t)...} {}
+
+  template <size_t N>
+  struct Storage {
+    T data_[N];
+  };
+  template <>
+  struct Storage<0> {};
+
+  // Using a union ensures that the default constructor doesn't initialize
+  // anything.
+  union {
+    Storage<N> storage_;
+  };
+};
+
+}  // namespace sus::containers
+
+namespace sus {
+using containers::Array;
+}
