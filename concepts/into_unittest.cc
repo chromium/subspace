bb2051405cbb6d2655f2731a5976cd05bfdb0df8
Dana Jansens <danakj@chromium.org>
2022-06-28 00:22:06 -0400

    remove pragma once in unittest

--- concepts/into_unittest.cc
+++ concepts/into_unittest.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#pragma once
-
 #include "concepts/into.h"
 
 #include "third_party/googletest/googletest/include/gtest/gtest.h"
