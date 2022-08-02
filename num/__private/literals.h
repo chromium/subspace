51f99649c6ae58a731a366d2ad10835f073578ab
Dana Jansens <danakj@chromium.org>
2022-08-01 20:17:24 -0400

    constexpr integer literals are noexcept

--- num/__private/literals.h
+++ num/__private/literals.h
@@ -21,12 +21,13 @@
 //
 // This obviously adds some runtime + codegen overhead. We could use a "numeric
 // literal operator template" and construct the number from a `char...`
-// template, which is what we used to do before moving to `consteval`. However
-// that triggers a different MSVC bug when used with any unary/binary operator
-// in a templated function:
+// template, which is what we used to do before moving to `consteval` (see
+// commit 531ac278f5b96a63b39332a0b87ef207e0d40575). However that triggers a
+// different MSVC bug when used with any unary/binary operator in a templated
+// function:
 // https://developercommunity.visualstudio.com/t/MSVC-Compiler-bug-with:-numeric-literal/10108160
 #define _sus__integer_literal(Name, T)                                      \
-  T inline constexpr operator""_##Name(unsigned long long val) {            \
+  T inline constexpr operator""_##Name(unsigned long long val) noexcept {   \
     ::sus::check(val <= static_cast<unsigned long long>(T::MAX_PRIMITIVE)); \
     return T(static_cast<decltype(T::primitive_value)>(val));               \
   }
