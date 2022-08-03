4e620746e886c2309858483a3d0322542d1e2574
Dana Jansens <danakj@chromium.org>
2022-08-02 20:19:47 -0400

    Improve the doc comment on integer literals

--- num/__private/literals.h
+++ num/__private/literals.h
@@ -15,23 +15,26 @@
 #pragma once
 
 #if _MSC_VER
-// Workaround for MSVC bug that doesn't constant-evaluate user-defined literals
-// in all cases:
-// https://developercommunity.visualstudio.com/t/User-defined-literals-not-constant-expre/10108165
-//
-// This obviously adds some runtime + codegen overhead. We could use a "numeric
-// literal operator template" and construct the number from a `char...`
-// template, which is what we used to do before moving to `consteval` (see
-// commit 531ac278f5b96a63b39332a0b87ef207e0d40575). However that triggers a
-// different MSVC bug when used with any unary/binary operator in a templated
-// function:
-// https://developercommunity.visualstudio.com/t/MSVC-Compiler-bug-with:-numeric-literal/10108160
-#define _sus__integer_literal(Name, T)                                      \
-  T inline constexpr operator""_##Name(unsigned long long val) noexcept {   \
-    ::sus::check(val <= static_cast<unsigned long long>(T::MAX_PRIMITIVE)); \
-    return T(static_cast<decltype(T::primitive_value)>(val));               \
+/// Literal integer value.
+#define _sus__integer_literal(Name, T)                                                               \
+  /* A `constexpr` workaround for MSVC bug that doesn't constant-evaluate                            \
+   * user-defined literals in all cases:                                                             \
+   * https://developercommunity.visualstudio.com/t/User-defined-literals-not-constant-expre/10108165 \
+   *                                                                                                 \
+   * This obviously adds some runtime + codegen overhead. We could use a                             \
+   * "numeric literal operator template" and construct the number from a                             \
+   * `char...` template, which is what we used to do before moving to                                \
+   * `consteval` (see commit 531ac278f5b96a63b39332a0b87ef207e0d40575).                              \
+   * However that triggers a different MSVC bug when used with any                                   \
+   * unary/binary operator in a templated function:                                                  \
+   * https://developercommunity.visualstudio.com/t/MSVC-Compiler-bug-with:-numeric-literal/10108160  \
+   */                                                                                                \
+  T inline constexpr operator""_##Name(unsigned long long val) noexcept {                            \
+    ::sus::check(val <= static_cast<unsigned long long>(T::MAX_PRIMITIVE));                          \
+    return T(static_cast<decltype(T::primitive_value)>(val));                                        \
   }
 #else
+/// Literal integer value.
 #define _sus__integer_literal(Name, T)                           \
   T inline consteval operator""_##Name(unsigned long long val) { \
     if (val > static_cast<unsigned long long>(T::MAX_PRIMITIVE)) \
