74b5c8c5ac56d3d1ab7cafb8322b0a6fa5144310
Dana Jansens <danakj@chromium.org>
2022-11-01 22:04:28 -0400

    Document the never-value trait type

--- mem/never_value.h
+++ mem/never_value.h
@@ -24,6 +24,13 @@
 
 namespace sus::mem {
 
+/// A trait to inspect if a type `T` has a field with a never-value. For such a
+/// type, it is possible to tell if the type is constructed in a memory
+/// location, by storing the never-value through `set_never_value()` in the
+/// memory location before it is constructed and/or after it is destroyed.
+///
+/// This allows a flag to check for a class being constructed without an
+/// additional boolean flag.
 template <class T>
 struct never_value_field {
  private:
@@ -39,17 +46,31 @@ struct never_value_field {
  public:
   static constexpr bool has_field = has_field_helper(0);
 
+  /// Returns whether there is a type `T` constructed at the memory location
+  /// `t`.
+  ///
+  /// # Safety
+  /// This will only produce a correct answer if the memory was previous set to
+  /// the never-value through `set_never_value()` before construction of the
+  /// type `T`.
   static constexpr sus_always_inline bool is_constructed(
-      ::sus::marker::UnsafeFnMarker, const T& t) noexcept
+      ::sus::marker::UnsafeFnMarker, const T* t) noexcept
     requires(has_field)
   {
-    return t.SusUnsafeNeverValueIsConstructed();
+    return t->SusUnsafeNeverValueIsConstructed();
   }
+  /// Sets a field in the memory location `t` to a value that is never set
+  /// during the lifetime of `T`.
+  ///
+  /// # Safety
+  /// This must never be called while there is an object of type `T` constructed
+  /// at the given memory location. It must be called only before a constructor
+  /// is run, or after a destructor is run.
   static constexpr sus_always_inline void set_never_value(
-      ::sus::marker::UnsafeFnMarker, Mref<T&> t) noexcept
+      ::sus::marker::UnsafeFnMarker, T* t) noexcept
     requires(has_field)
   {
-    t.inner().SusUnsafeNeverValueSetNeverValue();
+    t->SusUnsafeNeverValueSetNeverValue();
   }
 };
 
@@ -62,8 +83,9 @@ struct never_value_field {
 #define sus_class_never_value_field(unsafe_fn, T, name, never_value)           \
   static_assert(                                                               \
       std::same_as<decltype(unsafe_fn), const ::sus::marker::UnsafeFnMarker>); \
+  static_assert(std::is_assignable_v<decltype(name)&, decltype(never_value)>); \
   template <class SusOuterClassTypeForNeverValueField>                         \
-  friend struct ::sus::mem::never_value_field;                         \
+  friend struct ::sus::mem::never_value_field;                                 \
   constexpr sus_always_inline bool SusUnsafeNeverValueIsConstructed()          \
       const noexcept {                                                         \
     static_assert(std::same_as<decltype(*this), const T&>);                    \
--- option/__private/storage.h
+++ option/__private/storage.h
@@ -24,8 +24,8 @@ namespace sus::option::__private {
 using State::None;
 using State::Some;
 
-template <class T, bool HasNonNullField =
-                       sus::mem::never_value_field<T>::has_field>
+template <class T,
+          bool HasNonNullField = sus::mem::never_value_field<T>::has_field>
 struct Storage;
 
 // TODO: Determine if we can put the State into the storage of `T`. Probably
@@ -124,8 +124,7 @@ struct Storage<T, true> final {
   = default;
 
   constexpr Storage() {
-    ::sus::mem::never_value_field<T>::set_never_value(unsafe_fn,
-                                                              mref(val_));
+    ::sus::mem::never_value_field<T>::set_never_value(unsafe_fn, &val_);
   }
   constexpr Storage(const T& t) : val_(t) {}
   constexpr Storage(T&& t) : val_(static_cast<T&&>(t)) {}
@@ -135,8 +134,7 @@ struct Storage<T, true> final {
   };
 
   [[nodiscard]] constexpr inline State state() const noexcept {
-    return ::sus::mem::never_value_field<T>::is_constructed(unsafe_fn,
-                                                                    val_)
+    return ::sus::mem::never_value_field<T>::is_constructed(unsafe_fn, &val_)
                ? Some
                : None;
   }
@@ -158,15 +156,13 @@ struct Storage<T, true> final {
 
   [[nodiscard]] constexpr inline T take_and_set_none() noexcept {
     T t = take_and_destruct(unsafe_fn, mref(val_));
-    ::sus::mem::never_value_field<T>::set_never_value(unsafe_fn,
-                                                              mref(val_));
+    ::sus::mem::never_value_field<T>::set_never_value(unsafe_fn, &val_);
     return t;
   }
 
   constexpr inline void set_none() noexcept {
     val_.~T();
-    ::sus::mem::never_value_field<T>::set_never_value(unsafe_fn,
-                                                              mref(val_));
+    ::sus::mem::never_value_field<T>::set_never_value(unsafe_fn, &val_);
   }
 };
 
