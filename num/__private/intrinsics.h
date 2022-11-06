0726ae1d397b9572bf4b61311281a68ce606237f
Dana Jansens <danakj@chromium.org>
2022-11-06 17:07:55 -0500

    Specify order of operations for & and !=

--- num/__private/intrinsics.h
+++ num/__private/intrinsics.h
@@ -535,13 +535,13 @@ template <class T>
   requires(sizeof(T) <= 8)
 sus_always_inline constexpr bool sign_bit(T x) noexcept {
   if constexpr (sizeof(x) == 1)
-    return x & (T(1) << 7) != 0;
+    return (x & (T(1) << 7)) != 0;
   else if constexpr (sizeof(x) == 2)
-    return x & (T(1) << 15) != 0;
+    return (x & (T(1) << 15)) != 0;
   else if constexpr (sizeof(x) == 4)
-    return x & (T(1) << 31) != 0;
+    return (x & (T(1) << 31)) != 0;
   else
-    return x & (T(1) << 63) != 0;
+    return (x & (T(1) << 63)) != 0;
 }
 
 template <class T>
