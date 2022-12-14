cc890ad0d5302a9a67c9c9150456dbe08f581ef4
danakj <danakj@chromium.org>
2022-12-14 13:56:34 -0500

    We make some architecture assumptions on size of char, int, size_t

--- lib/lib.cc
+++ lib/lib.cc
@@ -11,3 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#include <stddef.h>
+#include <limits.h>
+
+// Architectural assumptions we make throughout the implementation of Subspace.
+static_assert(CHAR_BIT == 8);
+static_assert(sizeof(int) == 4);
+static_assert(sizeof(size_t) >= 4);
+static_assert(sizeof(size_t) <= 8);
