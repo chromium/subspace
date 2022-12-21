582c5498852721467d5888afea5e4168955eb991
Dana Jansens <danakj@chromium.org>
2022-12-20 23:22:58 -0500

    Better error in forward

--- mem/forward.h
+++ mem/forward.h
@@ -22,10 +22,11 @@ template <class T>
 constexpr T&& forward(std::remove_reference_t<T>& t) noexcept {
   return static_cast<T&&>(t);
 }
+
 template <class T>
 constexpr T&& forward(std::remove_reference_t<T>&& t) noexcept {
-  static_assert(!std::is_lvalue_reference<T>::value,
-                "Can not forward an rvalue as an lvalue.");
+  static_assert(!std::is_lvalue_reference_v<T>,
+                "Can not convert an rvalue to an lvalue with forward().");
   return static_cast<T&&>(t);
 }
 
