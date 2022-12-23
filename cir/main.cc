a858423c7e556a1ca5493fd8f266ed7a71fef7cf
Dana Jansens <danakj@chromium.org>
2022-12-23 18:14:22 -0500

    Add license, fix/hide build warnings.

--- cir/main.cc
+++ cir/main.cc
@@ -1,9 +1,26 @@
-#include <iostream>
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+#pragma warning(push)
+#pragma warning(disable : 4624)
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/WithColor.h"
+#pragma warning(pop)
+
 #include "subspace/prelude.h"
 
 int main(int argc, const char** argv) {
@@ -16,6 +33,10 @@ int main(int argc, const char** argv) {
   llvm::Expected<clang::tooling::CommonOptionsParser> options_parser =
       clang::tooling::CommonOptionsParser::create(argc, argv, option_category,
                                                   llvm::cl::ZeroOrMore);
+  if (!options_parser) {
+    llvm::WithColor::error() << llvm::toString(options_parser.takeError());
+    return 1;
+  }
 
-  std::cout << "hello world\n";
+  llvm::outs() << "hello world";
 }
