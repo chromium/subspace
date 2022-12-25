6c5660c82937f5da62edc3bacc2eaea776dc836f
Dana Jansens <danakj@chromium.org>
2022-12-24 21:01:35 -0500

    Make a single header that includes all LLVM/Clang things we use
    Since we want to compile with warnings on and as errors, and
    LLVM headers have a lot of warnings in them. This gives us a
    place to disable them just for LLVM headers.

---
+++ cir/llvm.h
@@ -0,0 +1,44 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+// All LLVM and Clang includes go here, because they are full of compiler
+// warnings that we have to disable.
+
+#pragma warning(push)
+#pragma warning(disable : 4100)
+#pragma warning(disable : 4127)
+#pragma warning(disable : 4146)
+#pragma warning(disable : 4244)
+#pragma warning(disable : 4245)
+#pragma warning(disable : 4267)
+#pragma warning(disable : 4291)
+#pragma warning(disable : 4324)
+#pragma warning(disable : 4389)
+#pragma warning(disable : 4458)
+#pragma warning(disable : 4459)
+#pragma warning(disable : 4616)
+#pragma warning(disable : 4624)
+
+#include "clang/AST/DeclBase.h"
+#include "clang/Frontend/ASTUnit.h"
+#include "clang/Serialization/PCHContainerOperations.h"
+#include "clang/Tooling/CompilationDatabase.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/ADT/IntrusiveRefCntPtr.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/VirtualFileSystem.h"
+
+#pragma warning(pop)
