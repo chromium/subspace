e89af0ecd26299f65de0ae054f2f939fc210f75f
Dana Jansens <danakj@chromium.org>
2023-01-06 10:41:22 -0500

    Introduce a shell of SubDoc.

---
+++ subdoc/lib/run.cc
@@ -0,0 +1,100 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "subdoc/lib/run.h"
+
+#include <memory>
+#include <utility>
+
+#include "subdoc/lib/visit.h"
+#include "subspace/iter/iterator.h"
+
+namespace subdoc {
+
+sus::Option<Database> run_test(std::string content,
+                               sus::Vec<std::string> args) noexcept {
+  auto join_args = std::string();
+  for (const auto& a : sus::move(args).into_iter()) {
+    join_args += a + "\n";
+  }
+
+  auto err = std::string();
+  auto comp_db = clang::tooling::FixedCompilationDatabase::loadFromBuffer(
+      ".", join_args, err);
+  if (!err.empty()) {
+    llvm::outs() << "error making comp_db for tests: " << err << "\n";
+    return sus::none();
+  }
+
+  auto vfs = llvm::IntrusiveRefCntPtr(new llvm::vfs::InMemoryFileSystem());
+  vfs->addFile("test.cc", 0, llvm::MemoryBuffer::getMemBuffer(content));
+
+  return run_files(*comp_db, sus::vec("test.cc"), std::move(vfs));
+}
+
+sus::Option<Database> run_files(
+    const clang::tooling::CompilationDatabase& comp_db,
+    sus::Vec<std::string> paths,
+    llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> fs) noexcept {
+  // Clang DiagnoticsConsumer that prints out the full error and context, which
+  // is what the default one does, but by making it we have a pointer from which
+  // we can see if an error occured.
+  auto diags = std::make_unique<clang::TextDiagnosticPrinter>(
+      llvm::errs(), new clang::DiagnosticOptions());
+
+  // TODO: Implement this efficiently in subspace.
+  auto to_std_vec = []<class T>(sus::Vec<T> v) {
+    std::vector<T> s;
+    s.reserve(size_t{v.len()});
+    for (auto&& e : sus::move(v).into_iter())
+      s.push_back(sus::move(e));
+    return s;
+  };
+
+  auto tool = clang::tooling::ClangTool(
+      comp_db, to_std_vec(sus::move(paths)),
+      std::make_shared<clang::PCHContainerOperations>(), std::move(fs));
+  tool.setDiagnosticConsumer(&*diags);
+
+  auto adj = [](clang::tooling::CommandLineArguments args,
+                llvm::StringRef Filename) {
+    // Clang-cl doesn't understand this argument, but it may appear in the
+    // command-line for MSVC in C++20 codebases (like subspace).
+    std::erase(args, "/Zc:preprocessor");
+
+    const std::string& tool = args[0];
+    if (tool.find("cl.exe") != std::string::npos) {
+      // TODO: https://github.com/llvm/llvm-project/issues/59689 clang-cl
+      // requires this define in order to use offsetof() from constant
+      // expressions, which subspace uses for the never-value optimization.
+      args.push_back("/D_CRT_USE_BUILTIN_OFFSETOF");
+    }
+
+    return std::move(args);
+  };
+  tool.appendArgumentsAdjuster(adj);
+
+  auto cx = VisitCx();
+  auto docs_db = Database();
+  auto visitor_factory = VisitorFactory(cx, docs_db);
+  if (tool.run(&visitor_factory) == 1) {
+    return sus::none();
+  }
+  if (diags->getNumErrors() > 0) {
+    return sus::none();
+  }
+  return sus::some(sus::move(docs_db));
+}
+
+}  // namespace subdoc
---
+++ subdoc/lib/visit.cc
@@ -0,0 +1,102 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "subdoc/lib/visit.h"
+
+#include "subspace/assertions/unreachable.h"
+#include "subspace/prelude.h"
+
+namespace subdoc {
+
+class Visitor : public clang::RecursiveASTVisitor<Visitor> {
+ public:
+  Visitor(VisitCx& cx, Database& docs_db) : cx_(cx), docs_db_(docs_db) {}
+  bool shouldVisitLambdaBody() const { return false; }
+
+  bool VisitStaticAssertDecl(clang::StaticAssertDecl* decl) noexcept {
+    llvm::errs() << "StaticAssertDecl\n";
+    return true;
+  }
+  bool VisitNamespaceDecl(clang::NamespaceDecl* decl) noexcept {
+    clang::RawComment* raw_comment = get_raw_comment(*decl);
+    if (!raw_comment) return true;
+    return true;
+  }
+  bool VisitRecordDecl(clang::RecordDecl* decl) noexcept {
+    clang::RawComment* raw_comment = get_raw_comment(*decl);
+    if (!raw_comment) return true;
+    llvm::errs() << "RecordDecl " << raw_comment->getKind() << "\n";
+    return true;
+  }
+  bool VisitEnumDecl(clang::EnumDecl* decl) noexcept {
+    clang::RawComment* raw_comment = get_raw_comment(*decl);
+    if (!raw_comment) return true;
+    return true;
+    llvm::errs() << "EnumDecl " << raw_comment->getKind() << "\n";
+  }
+  bool VisitTypedefDecl(clang::TypedefDecl* decl) noexcept {
+    clang::RawComment* raw_comment = get_raw_comment(*decl);
+    if (!raw_comment) return true;
+    return true;
+    llvm::errs() << "TypedefDecl " << raw_comment->getKind() << "\n";
+  }
+  bool VisitTypeAliasDecl(clang::TypeAliasDecl* decl) noexcept {
+    clang::RawComment* raw_comment = get_raw_comment(*decl);
+    if (!raw_comment) return true;
+    return true;
+    llvm::errs() << "TypeAliasDecl " << raw_comment->getKind() << "\n";
+  }
+  bool VisitFunctionDecl(clang::FunctionDecl* decl) noexcept {
+    clang::RawComment* raw_comment = get_raw_comment(*decl);
+    if (!raw_comment) return true;
+    llvm::errs() << "FunctionDecl " << raw_comment->getKind() << "\n";
+    return true;
+  }
+
+ private:
+  clang::RawComment* get_raw_comment(clang::Decl& decl) const {
+    return decl.getASTContext().getRawCommentForDeclNoCache(&decl);
+  }
+
+  VisitCx& cx_;
+  Database& docs_db_;
+};
+
+class AstConsumer : public clang::ASTConsumer {
+ public:
+  AstConsumer(VisitCx& cx, Database& docs_db) : cx_(cx), docs_db_(docs_db) {}
+
+  bool HandleTopLevelDecl(clang::DeclGroupRef group_ref) noexcept override {
+    for (clang::Decl* decl : group_ref) {
+      if (!Visitor(cx_, docs_db_).TraverseDecl(decl)) return false;
+    }
+    return true;
+  }
+
+ private:
+  VisitCx& cx_;
+  Database& docs_db_;
+};
+
+std::unique_ptr<clang::FrontendAction> VisitorFactory::create() noexcept {
+  return std::make_unique<VisitorAction>(cx, docs_db);
+}
+
+std::unique_ptr<clang::ASTConsumer> VisitorAction::CreateASTConsumer(
+    clang::CompilerInstance& compiler, llvm::StringRef file) noexcept {
+  // set preprocessor options?
+  return std::make_unique<AstConsumer>(cx, docs_db);
+}
+
+}  // namespace subdoc
