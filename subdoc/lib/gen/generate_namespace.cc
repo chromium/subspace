a8b8048740ca8f30dfb8b948a2b92c7a3cf3e06d
danakj <danakj@chromium.org>
2023-01-16 20:08:54 -0500

    Generate nested namespaces and link to them

--- subdoc/lib/gen/generate_namespace.cc
+++ subdoc/lib/gen/generate_namespace.cc
@@ -27,8 +27,18 @@ namespace subdoc::gen {
 
 namespace {
 
+std::string namespace_display_name(const NamespaceElement& element) noexcept {
+  // The namespace path includes the namespace we're generating for, so drop
+  // that one.
+  sus::Slice<const Namespace> short_namespace_path =
+      element.namespace_path.as_ref()[{1u, element.namespace_path.len() - 1u}];
+
+  // For display in the html, we use the full path name of the namespace.
+  return namespace_with_path_to_string(short_namespace_path,
+                                       element.namespace_name);
+}
+
 void generate_namespace_overview(HtmlWriter::OpenDiv& namespace_div,
-                                 std::string_view name,
                                  const NamespaceElement& element) {
   auto section_div = namespace_div.open_div();
   section_div.add_class("section");
@@ -46,7 +56,49 @@ void generate_namespace_overview(HtmlWriter::OpenDiv& namespace_div,
       auto name_anchor = header_div.open_a();
       name_anchor.add_href("#");
       name_anchor.add_class("namespace-name");
-      name_anchor.write_text(name);
+      name_anchor.write_text(namespace_display_name(element));
+    }
+    if (element.has_comment()) {
+      auto desc_div = header_div.open_div();
+      desc_div.add_class("description");
+      desc_div.write_text(element.comment.raw_text);
+    }
+  }
+}
+
+void generate_namespace_namespaces(
+    HtmlWriter::OpenDiv& namespace_div, const NamespaceElement& element,
+    sus::Slice<const sus::Tuple<std::string_view, UniqueSymbol>> namespaces) {
+  if (namespaces.is_empty()) return;
+
+  auto section_div = namespace_div.open_div();
+  section_div.add_class("section");
+  section_div.add_class("namespaces");
+
+  {
+    auto functions_header_div = section_div.open_div();
+    functions_header_div.add_class("section-header");
+    functions_header_div.write_text("Namespaces");
+  }
+  {
+    for (auto&& [name, uniq] : namespaces) {
+      const NamespaceElement& sub_element = element.namespaces.at(uniq);
+
+      auto item_div = section_div.open_div();
+      item_div.add_class("section-item");
+
+      {
+        auto name_link = item_div.open_a();
+        name_link.add_class("namespace-name");
+        name_link.add_href(std::string(construct_html_file_path_for_namespace(
+            std::filesystem::path(), sub_element)));
+        name_link.write_text(sub_element.name);
+      }
+      if (sub_element.has_comment()) {
+        auto desc_div = item_div.open_div();
+        desc_div.add_class("description");
+        desc_div.write_text(sub_element.comment.raw_text);
+      }
     }
   }
 }
@@ -73,56 +125,52 @@ void generate_namespace_functions(
     }
   }
 }
+
 }  // namespace
 
 void generate_namespace(const NamespaceElement& element,
                         const Options& options) noexcept {
-  // The namespace path includes the namespace we're generating for, so drop
-  // that one.
-  sus::Slice<const Namespace> short_namespace_path =
-      element.namespace_path.as_ref()[{1u, element.namespace_path.len() - 1u}];
-
-  // For display in the html, we use the full path name of the namespace.
-  std::string name = namespace_with_path_to_string(short_namespace_path,
-                                                   element.namespace_name);
-  std::string name_for_file = [&]() {
-    if (element.namespace_name.which() == Namespace::Tag::Global) {
-      // We're generating the global namespace, which will go in
-      // `global-ns.html`. Namespaces can't be named `namespace` so this can't
-      // collide with a real namespace `global::namespace`.
-      return std::string("global-namespace");
-    } else {
-      // Otherwise, just use the local name of the namespace.
-      return element.name;
-    }
-  }();
-
   const std::filesystem::path path =
-      construct_html_file_path(options.output_root, short_namespace_path,
-                               sus::Slice<const std::string>(), name_for_file);
+      construct_html_file_path_for_namespace(options.output_root, element);
   std::filesystem::create_directories(path.parent_path());
-  auto html = HtmlWriter(open_file_for_writing(path).unwrap());
 
-  generate_head(html, name, options);
+  auto html = HtmlWriter(open_file_for_writing(path).unwrap());
+  generate_head(html, namespace_display_name(element), options);
 
   auto body = html.open_body();
 
   auto namespace_div = body.open_div();
   namespace_div.add_class("namespace");
-  generate_namespace_overview(mref(namespace_div), name, element);
+  generate_namespace_overview(mref(namespace_div), element);
 
-  sus::Vec<sus::Tuple<std::string_view, const FunctionId&>> sorted_functions;
-  for (const auto& [function_id, function_element] : element.functions) {
-    sorted_functions.push(sus::tuple(function_element.name, function_id));
+  {
+    sus::Vec<sus::Tuple<std::string_view, UniqueSymbol>> sorted;
+    for (const auto& [uniq, element] : element.namespaces) {
+      sorted.push(sus::tuple(element.name, uniq));
+    }
+    sorted.sort_unstable_by(
+        [](const sus::Tuple<std::string_view, UniqueSymbol>& a,
+           const sus::Tuple<std::string_view, UniqueSymbol>& b) {
+          return a.get_ref<0>() <=> b.get_ref<0>();
+        });
+
+    generate_namespace_namespaces(mref(namespace_div), element,
+                                  sorted.as_ref());
   }
-  sorted_functions.sort_unstable_by(
-      [](const sus::Tuple<std::string_view, const FunctionId&>& a,
-         const sus::Tuple<std::string_view, const FunctionId&>& b) {
-        return a.get_ref<0>() <=> b.get_ref<0>();
-      });
 
-  generate_namespace_functions(mref(namespace_div), element,
-                               sorted_functions.as_ref());
+  {
+    sus::Vec<sus::Tuple<std::string_view, const FunctionId&>> sorted;
+    for (const auto& [function_id, element] : element.functions) {
+      sorted.push(sus::tuple(element.name, function_id));
+    }
+    sorted.sort_unstable_by(
+        [](const sus::Tuple<std::string_view, const FunctionId&>& a,
+           const sus::Tuple<std::string_view, const FunctionId&>& b) {
+          return a.get_ref<0>() <=> b.get_ref<0>();
+        });
+
+    generate_namespace_functions(mref(namespace_div), element, sorted.as_ref());
+  }
 
   // Recurse into namespaces and records.
   for (const auto& [u, element] : element.namespaces) {
