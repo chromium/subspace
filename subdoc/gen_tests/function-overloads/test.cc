810e8de2ec3fa9c40f1647c403058e00f5b4b367
Dana Jansens <danakj@chromium.org>
2023-01-26 21:03:47 -0500

    Adjust the parsing and syntax for doc.overloads and test merging named documentation overload sets

--- subdoc/gen_tests/function-overloads/test.cc
+++ subdoc/gen_tests/function-overloads/test.cc
@@ -3,10 +3,11 @@ namespace n {
 /// Function with overloads
 void multiple();
 void multiple(int);
+/// #[doc.overloads=0]  // This is merged with the default overload set.
 void multiple(float);
 
 /// Separated overload
-/// #[doc(overloads=1)]
+/// #[doc.overloads=1]
 void multiple(unsigned);
 
 }  // namespace n
