d913e82c646cf36a3e9e82edf058ff9e425b40e4
Dana Jansens <danakj@chromium.org>
2023-02-02 22:05:50 -0500

    Document the UnsafeFnMarker

--- subspace/marker/unsafe.h
+++ subspace/marker/unsafe.h
@@ -16,12 +16,31 @@
 
 namespace sus::marker {
 
+/// A marker that designates a function as unsafe, or containing Undefined
+/// Behaviour if its preconditions are not met.
+///
+/// Use of an unsafe function should require a comment documenting how the
+/// required preconditions are met in the form:
+///
+/// ```
+/// // SAFETY: This is known to be true because of that which we checked there.
+/// do_risky_thing(unsafe_fn);
+/// ```
+///
+/// Input conditions of the unsafe function should be well encapsulated so that
+/// it is even possible to reason about how they are met and to maintain that
+/// over time.
+///
+/// To call such an unsafe function, pass it the global `unsafe_fn` object,
+/// which is brought into the global namespace by "subspace/prelude.h".
 struct UnsafeFnMarker {
   // `explicit` prevents `{}` or `{any values...}` from being used to construct
   // an `UnsafeFnMarker`. The marker should only be used as `unsafe_fn`.
   explicit consteval UnsafeFnMarker() {}
 };
 
+/// The global `UnsafeFnMarker` which can be passed to unsafe functions. See
+/// the `UnsafeFnMarker` type for an explanation.
 constexpr inline auto unsafe_fn = UnsafeFnMarker();
 
 }  // namespace sus::marker
