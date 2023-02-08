c9164d50a72f2c96c025aa42b4d09933c08cabf4
danakj <danakj@chromium.org>
2023-02-08 10:34:10 -0500

    Static assert that alignment is reasonable in data_size_of()
    Otherwise, the returned data size is 0, which doesn't make sense and
    then marks types as not trivially relocatable in confusing ways.
    
    Add test coverage for the final types codepaths of data_size_of()
    that were added in the process of debugging.

--- subspace/mem/size_of_unittest.cc
+++ subspace/mem/size_of_unittest.cc
@@ -103,6 +103,130 @@ TEST(DataSizeOf, NonEmptyClass) {
   }
 }
 
+struct NonStandardFinal final {
+  i64 a;
+  // A public and private field makes the class non-standard-layout so the
+  // padding can be re-purposed.
+ private:
+  i32 b;
+};
+static_assert(!std::is_standard_layout_v<NonStandardFinal>);
+
+struct NonTrivialFinal final {
+  // The non-trivial constructor makes X non-trivial so the padding can
+  // be re-purposed..
+  NonTrivialFinal(i32) {}
+  i64 a;
+  i32 b;
+};
+static_assert(!std::is_trivial_v<NonTrivialFinal>);
+
+struct HoldNonStandardFinal {
+  [[sus_no_unique_address]] NonStandard x;
+  i32 c;
+};
+
+struct HoldNonTrivialFinal {
+  [[sus_no_unique_address]] NonTrivial x;
+  i32 c;
+};
+
+TEST(DataSizeOf, NonEmptyFinalClass) {
+  if constexpr (sus_if_msvc_else(true, false)) {
+    // On MSVC, the class with a [[no_unique_address]] field does not use the
+    // padding at the end of that field.
+    //
+    // This is confusing because supposedly [[msvc::no_unique_address]] opts
+    // into this attribute actually doing something, but it doesn't appear to
+    // actually do so.
+    EXPECT_GT(sizeof(HoldNonStandardFinal), sizeof(NonStandard));
+    EXPECT_GT(sizeof(HoldNonTrivialFinal), sizeof(NonTrivial));
+    // As a result, the data_size_of<X>() includes the padding too, as other
+    // types can not be put into the padding.
+    EXPECT_EQ(sus::size_of<NonStandardFinal>(), sizeof(i64) + sizeof(i32) * 2);
+    EXPECT_EQ(sus::size_of<NonTrivialFinal>(), sizeof(i64) + sizeof(i32) * 2);
+    EXPECT_EQ(sus::data_size_of<NonStandardFinal>(),
+              sizeof(i64) + sizeof(i32) * 2);
+    EXPECT_EQ(sus::data_size_of<NonTrivialFinal>(),
+              sizeof(i64) + sizeof(i32) * 2);
+  } else {
+    // On other compilers, a [[no_unique_address]] field will have its tail
+    // padding used, so the `data_size_of<NoUniqueAddressClass>()` only covers
+    // its fields and not its padding.
+    EXPECT_EQ(sizeof(HoldNonStandardFinal), sizeof(NonStandard));
+    EXPECT_EQ(sizeof(HoldNonTrivialFinal), sizeof(NonTrivial));
+    EXPECT_EQ(sus::size_of<NonStandardFinal>(), sizeof(i64) + sizeof(i32) * 2);
+    EXPECT_EQ(sus::size_of<NonTrivialFinal>(), sizeof(i64) + sizeof(i32) * 2);
+    EXPECT_EQ(sus::data_size_of<NonStandardFinal>(), sizeof(i64) + sizeof(i32));
+    EXPECT_EQ(sus::data_size_of<NonTrivialFinal>(), sizeof(i64) + sizeof(i32));
+  }
+}
+
+// For testing non-final classes.
+
+struct SmallerThanMaxAlignNoTailPadding {
+  i32 i;
+};
+// The goal is to have a type that is larger than the max alignment. If this
+// fails for some future platform, make the type larger (without tail padding)
+// to match.
+static_assert(sizeof(SmallerThanMaxAlignNoTailPadding) <
+              alignof(std::max_align_t));
+
+struct MaxAlignNoTailPadding {
+  alignas(alignof(std::max_align_t)) char i[alignof(std::max_align_t)];
+};
+// The goal is to have a type that is the size of the max alignment. If this
+// fails for some future platform, make the type larger (without tail padding)
+// to match.
+static_assert(sizeof(MaxAlignNoTailPadding) == alignof(std::max_align_t));
+
+struct LargerThanMaxAlignNoTailPadding {
+  alignas(alignof(std::max_align_t)) char i[alignof(std::max_align_t)];
+  alignas(alignof(std::max_align_t)) char j[alignof(std::max_align_t)];
+};
+// The goal is to have a type that is larger than the max alignment. If this
+// fails for some future platform, make the type larger (without tail padding)
+// to match.
+static_assert(sizeof(LargerThanMaxAlignNoTailPadding) >
+              alignof(std::max_align_t));
+
+// For testing final classes.
+
+struct SmallerThanMaxAlignNoTailPaddingFinal final {
+  i32 i;
+};
+// Same as SmallerThanMaxAlignNoTailPadding, but also `final`.
+static_assert(sizeof(SmallerThanMaxAlignNoTailPaddingFinal) <
+              alignof(std::max_align_t));
+struct MaxAlignNoTailPaddingFinal final {
+  alignas(alignof(std::max_align_t)) char i[alignof(std::max_align_t)];
+};
+// Same as MaxAlignNoTailPadding, but also `final`.
+static_assert(sizeof(MaxAlignNoTailPaddingFinal) == alignof(std::max_align_t));
+struct LargerThanMaxAlignNoTailPaddingFinal final {
+  alignas(alignof(std::max_align_t)) char i[alignof(std::max_align_t)];
+  alignas(alignof(std::max_align_t)) char j[alignof(std::max_align_t)];
+};
+// Same as LargerThanMaxAlignNoTailPaddingFinal, but also `final`.
+static_assert(sizeof(LargerThanMaxAlignNoTailPaddingFinal) >
+              alignof(std::max_align_t));
+
+TEST(DataSizeOf, NoTailPadding) {
+  EXPECT_EQ(sus::data_size_of<SmallerThanMaxAlignNoTailPadding>(),
+            sus::size_of<SmallerThanMaxAlignNoTailPadding>());
+  EXPECT_EQ(sus::data_size_of<MaxAlignNoTailPadding>(),
+            sus::size_of<MaxAlignNoTailPadding>());
+  EXPECT_EQ(sus::data_size_of<LargerThanMaxAlignNoTailPadding>(),
+            sus::size_of<LargerThanMaxAlignNoTailPadding>());
+  EXPECT_EQ(sus::data_size_of<SmallerThanMaxAlignNoTailPaddingFinal>(),
+            sus::size_of<SmallerThanMaxAlignNoTailPaddingFinal>());
+  EXPECT_EQ(sus::data_size_of<MaxAlignNoTailPaddingFinal>(),
+            sus::size_of<MaxAlignNoTailPaddingFinal>());
+  EXPECT_EQ(sus::data_size_of<LargerThanMaxAlignNoTailPaddingFinal>(),
+            sus::size_of<LargerThanMaxAlignNoTailPaddingFinal>());
+}
+
 struct Empty {};
 struct SubEmpty {
   char c;
