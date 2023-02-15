e452392d9f3eb762a4c5af81f070df645451a87d
Dana Jansens <danakj@chromium.org>
2023-02-14 22:09:11 -0500

    Introduce the IntoIterator iterator_concept
    Iterators are always also IntoIterator, returning a reference to
    themselves.
    
    This allows code to receive an iterator or a container, and is
    most useful when receiving an iterator over non-reference values.

--- subspace/iter/iterator_concept.h
+++ subspace/iter/iterator_concept.h
@@ -14,14 +14,17 @@
 
 #pragma once
 
+#include <type_traits>
+
 #include "subspace/convert/subclass.h"
+#include "subspace/mem/forward.h"
 
 namespace sus::iter {
 
 template <class Iter, class Item>
 class IteratorImpl;
 
-/// A concept for dealing with iterators.
+/// A concept for all implementations of iterators.
 ///
 /// Types that satisfy this concept can be used in for loops and provide
 /// all the methods of an iterator type, which are found in
@@ -29,11 +32,28 @@ class IteratorImpl;
 template <class T, class Item>
 concept Iterator = requires {
   // Has a T::Item typename.
-  requires(!std::is_void_v<typename T::Item>);
+  requires(!std::is_void_v<typename std::decay_t<T>::Item>);
   // The T::Item matches the concept input.
-  requires(std::same_as<typename T::Item, Item>);
+  requires(std::same_as<typename std::decay_t<T>::Item, Item>);
   // Subclasses from IteratorImpl<T, T::Item>.
-  requires ::sus::convert::SameOrSubclassOf<T*, IteratorImpl<T, Item>*>;
+  requires ::sus::convert::SameOrSubclassOf<
+      std::decay_t<T>*, IteratorImpl<std::decay_t<T>, Item>*>;
+};
+
+/// Conversion into an `Iterator`.
+///
+/// A more general trait than `Iterator` which will accept anything that can be
+/// iterated, including an `Iterator` (since all `Iterator`s also satisfy
+/// `IntoIterator`). This can be particularly useful when receiving an iterator
+/// over a set of non-reference values, allowing the caller to pass a container
+/// directly in place of an iterator.
+///
+/// Note that an `IntoIterator` type is not directly iterable in for loops, and
+/// requires calling `into_iter()` on it to convert it into an `Iterator`
+/// which is iterable in for loops.
+template <class T, class Item>
+concept IntoIterator = requires(T&& t) {
+  { ::sus::forward<T>(t).into_iter() } -> Iterator<Item>;
 };
 
 }  // namespace sus::iter
