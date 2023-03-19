9ab1cd51ed54526bb2a269eb6afb65b5466aaa08
Dana Jansens <danakj@chromium.org>
2023-03-19 16:09:02 -0400

    The Eq and Ord concepts and methods are in ops but should be in cmp

--- subspace/ops/eq.h
+++ subspace/ops/eq.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// TODO: Move all this to sus::cmp?
+
 #pragma once
 
 #include <concepts>
