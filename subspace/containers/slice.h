a99da83f7cd5de8380971b96784c19b1f3f7cc68
danakj <danakj@chromium.org>
2023-03-22 14:03:14 -0400

    Avoid ambiguous conversions to Slice from rvalues
    Converting from && to Slice&& is ambiguous with returning a const Slice&
    as the Slice type has to pick a copy or a move constructor.
    
    Instead we can convert from && to Slice (not a reference) which avoids
    needing to pick a constructor on the Slice type, we chose it in the
    conversion operator (the move one).

--- subspace/containers/slice.h
+++ subspace/containers/slice.h
@@ -215,7 +215,7 @@ class [[sus_trivial_abi]] SliceMut final {
   // SliceMut can be used as a Slice.
   operator const Slice<T>&() const& { return slice_; }
   operator Slice<T>&() & { return slice_; }
-  operator Slice<T>&&() && { return ::sus::move(slice_); }
+  operator Slice<T>() && { return ::sus::move(slice_); }
 
 #define _ptr_expr slice_.data_
 #define _len_expr slice_.len_
