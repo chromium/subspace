70c71c95d124131197b10d0aeaaf2d154704e351
Dana Jansens <danakj@chromium.org>
2023-04-07 22:14:45 -0400

    Remove Array::with_uninitialized
    It's a bad use of a union, we never make the one member
    active. Constexpr usage caught this.

--- subspace/num/__private/signed_integer_macros.h
+++ subspace/num/__private/signed_integer_macros.h
@@ -1820,8 +1820,7 @@ class Tuple;
                                                                               \
   constexpr ::sus::containers::Array<u8, Bytes> T::to_ne_bytes()              \
       const& noexcept {                                                       \
-    auto bytes = ::sus::containers::Array<u8, Bytes>::with_uninitialized(     \
-        ::sus::marker::unsafe_fn);                                            \
+    auto bytes = ::sus::containers::Array<u8, Bytes>();                       \
     if (std::is_constant_evaluated()) {                                       \
       auto uval = __private::into_unsigned(primitive_value);                  \
       for (auto i = size_t{0}; i < Bytes; ++i) {                              \
