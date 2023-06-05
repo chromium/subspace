7a20372a71863fc896fdda343e7271a9f3069498
Dana Jansens <danakj@chromium.org>
2023-06-05 16:54:15 -0400

    Add tests that we parse namespaces in various configurations

---
+++ subdoc/tests/namespaces_unittest.cc
@@ -0,0 +1,91 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "subdoc/tests/subdoc_test.h"
+
+TEST_F(SubDocTest, NamespaceTopLevel) {
+  auto result = run_code(R"(
+    /// Comment headline
+    void f() {}
+  )");
+  ASSERT_TRUE(result.is_ok());
+  subdoc::Database db = sus::move(result).unwrap();
+  EXPECT_TRUE(has_function_comment(db, "2:5", "<p>Comment headline</p>"));
+}
+
+TEST_F(SubDocTest, NamespaceSingle) {
+  auto result = run_code(R"(
+    namespace single {
+    /// Comment headline
+    void f() {}
+    }
+  )");
+  ASSERT_TRUE(result.is_ok());
+  subdoc::Database db = sus::move(result).unwrap();
+  EXPECT_TRUE(has_function_comment(db, "3:5", "<p>Comment headline</p>"));
+}
+
+TEST_F(SubDocTest, NamespaceNested) {
+  auto result = run_code(R"(
+    namespace single {
+    namespace nested {
+    /// Comment headline
+    void f() {}
+    }
+    }
+  )");
+  ASSERT_TRUE(result.is_ok());
+  subdoc::Database db = sus::move(result).unwrap();
+  EXPECT_TRUE(has_function_comment(db, "4:5", "<p>Comment headline</p>"));
+}
+
+TEST_F(SubDocTest, NamespaceNestedThenDots) {
+  auto result = run_code(R"(
+    namespace single {
+    namespace nested {
+    }
+    }
+    namespace single::nested {
+    /// Comment headline
+    void f() {}
+    }
+  )");
+  ASSERT_TRUE(result.is_ok());
+  subdoc::Database db = sus::move(result).unwrap();
+  EXPECT_TRUE(has_function_comment(db, "7:5", "<p>Comment headline</p>"));
+}
+
+TEST_F(SubDocTest, NamespaceDots) {
+  auto result = run_code(R"(
+    namespace single::nested {
+    /// Comment headline
+    void f() {}
+    }
+  )");
+  ASSERT_TRUE(result.is_ok());
+  subdoc::Database db = sus::move(result).unwrap();
+  EXPECT_TRUE(has_function_comment(db, "3:5", "<p>Comment headline</p>"));
+}
+
+TEST_F(SubDocTest, NamespaceTwoDots) {
+  auto result = run_code(R"(
+    namespace single::nested::more {
+    /// Comment headline
+    void f() {}
+    }
+  )");
+  ASSERT_TRUE(result.is_ok());
+  subdoc::Database db = sus::move(result).unwrap();
+  EXPECT_TRUE(has_function_comment(db, "3:5", "<p>Comment headline</p>"));
+}
