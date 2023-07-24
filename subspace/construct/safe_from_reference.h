7202b69268e6a39e5cbb359af2171bf6cb3d36fe
Dana Jansens <danakj@chromium.org>
2023-07-24 00:21:03 -0400

    Allow subclass references in marker types to convert to base class references

--- subspace/construct/safe_from_reference.h
+++ subspace/construct/safe_from_reference.h
@@ -14,23 +14,23 @@
 
 #pragma once
 
+#include "subspace/convert/subclass.h"
+
 namespace sus::construct {
 
 namespace __private {
 template <class T>
 concept IsConstLvalueReference = std::is_lvalue_reference_v<T> &&
                                  std::is_const_v<std::remove_reference_t<T>>;
-}
+
+}  // namespace __private
 
 /// Returns if a type `From` is safely constructible from a reference of type
-/// `To`. This is useful for marker types which hold a reference internally
-/// and are used to construct another type.
+/// `To`. This is useful for marker types which hold a reference internally and
+/// are used to construct another type.
 ///
-/// If `To` is a reference, then they types must match, a conversion would
-/// create a reference to a temporary.
-///
-/// If `To` is not a reference, then it needs to be constructible from `const
-/// From&`.
+/// If `To` is a const reference, then they types must match, as a conversion
+/// would create a reference to a temporary.
 ///
 /// This concept only produces a useful result when `From` is a reference type
 /// which may not outlive the storage of `To`, such as with a reference to a
@@ -38,7 +38,10 @@ concept IsConstLvalueReference = std::is_lvalue_reference_v<T> &&
 template <class To, class From>
 concept SafelyConstructibleFromReference =
     !__private::IsConstLvalueReference<To> ||
-    (std::same_as<std::remove_cvref_t<From>, std::remove_cvref_t<To>> &&
-     std::constructible_from<To, From>);
+    // If the type is the same then no temporary will be created.
+    std::same_as<std::remove_cvref_t<From>, std::remove_cvref_t<To>> ||
+    // If the type is a base class then no temporary will be created.
+    sus::convert::SameOrSubclassOf<std::remove_cvref_t<From>*,
+                                   std::remove_cvref_t<To>*>;
 
 }  // namespace sus::construct
