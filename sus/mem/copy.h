d7d5d075603313f361592e803cf21a468b1b5881
Dana Jansens <danakj@chromium.org>
2023-07-31 22:12:05 -0400

    IWYU: Point to prelude.h for the headers included from it

--- sus/mem/copy.h
+++ sus/mem/copy.h
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// IWYU pragma: private, include "sus/prelude.h"
+// IWYU pragma: friend "sus/.*"
 #pragma once
 
 #include <concepts>
