2635396509347ad63f29d5ec2163774e4c823646
Dana Jansens <danakj@chromium.org>
2023-08-11 21:12:16 -0400

    Add the project name to run_subdoc.bat

--- tools/run_subdoc.bat
+++ tools/run_subdoc.bat
@@ -1,1 +1,9 @@
-out\subdoc\subdoc -p out --out docs --include-file-pattern /sus/ --exclude-file-pattern /third_party/ --copy-file subdoc/gen_tests/subdoc-test-style.css --css subdoc-test-style.css --copy-file sus/index.html --project-md sus/project.md subspace/sus
+out\subdoc\subdoc -p out --out docs ^
+    --include-file-pattern /sus/ ^
+    --exclude-file-pattern /third_party/ ^
+    --css subdoc-test-style.css ^
+    --copy-file subdoc/gen_tests/subdoc-test-style.css ^
+    --copy-file sus/index.html ^
+    --project-md sus/project.md ^
+    --project-name Subspace ^
+    subspace/sus
