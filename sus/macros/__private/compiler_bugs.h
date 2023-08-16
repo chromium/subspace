df91f7417792f90309fe6e8216407a80cd77d494
Dana Jansens <danakj@orodu.net>
2023-08-16 14:53:10 -0400

    Fix GCC version

--- sus/macros/__private/compiler_bugs.h
+++ sus/macros/__private/compiler_bugs.h
@@ -141,7 +141,7 @@
 // GCC does not parse dependent types in a partial specialization.
 // Fixed in GCC 13.3.
 #if defined(__GNUC__) && \
-    (__GNUC__ > 13 || (__GNUC__ == 13 && __GNUC_MINOR__ >= 3))
+    (__GNUC__ < 13 || (__GNUC__ == 13 && __GNUC_MINOR__ < 3))
 #define sus_gcc_bug_110927(...) __VA_ARGS__
 #define sus_gcc_bug_110927_else(...)
 #define sus_gcc_bug_110927_exists
