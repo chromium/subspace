f512073489a97872c499202358a937f04c3d6dc2
Dana Jansens <danakj@chromium.org>
2023-08-18 13:33:22 -0400

    Linkify static data member types when possible, remove the link when not

--- subdoc/gen_tests/struct-complex/test.cc
+++ subdoc/gen_tests/struct-complex/test.cc
@@ -1,24 +1,26 @@
 // This tests static and non-static members. They are intermixed and will
 // come out grouped and in alphabetical order.
 
+struct OtherType {};
+
 /// Comment headline S
 struct S {
   /// Comment headline void_method
   void void_method() const& {}
-  /// Comment headline static_int_method
-  static int static_int_method() {}
-  /// Comment headline int_method
-  int int_method() {}
+  /// Comment headline static_typee_method
+  static OtherType static_type_method() {}
+  /// Comment headline type_method
+  OtherType type_method() {}
   /// Comment headline static_bool_method
   static bool static_bool_method() {}
 
   // Overload should be grouped with the other int_method().
   void int_method() volatile {}
 
-  /// Comment headline integer_field
-  const int integer_field;
-  /// Comment headline static_int_member
-  static volatile int static_int_member;
+  /// Comment headline type_field
+  const OtherType type_field;
+  /// Comment headline static_type_member
+  static volatile OtherType static_type_member;
   /// Comment headline bool_field
   bool bool_field;
   /// Comment headline static_bool_member
