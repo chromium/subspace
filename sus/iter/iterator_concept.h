69c9a0f11d08629509376f06c41807a6a26dedd2
Dana Jansens <danakj@orodu.net>
2023-09-01 16:39:38 -0400

    Improve docs for Iterator concepts

--- sus/iter/iterator_concept.h
+++ sus/iter/iterator_concept.h
@@ -25,12 +25,14 @@
 namespace sus::iter {
 
 /// A concept for all implementations of iterators.
+/// See [`IteratorBase`]($sus::iter::IteratorBase) for the methods on an
+/// `Iterator`.
 ///
 /// Types that satisfy this concept can be used in for loops and provide
 /// all the methods of an iterator type, which are found in
-/// `sus::iter::IteratorBase`.
+/// [`IteratorBase`]($sus::iter::IteratorBase).
 ///
-/// Any Iterator's full definition includes a number of other methods as well,
+/// Any `Iterator`'s full definition includes a number of other methods as well,
 /// built on top of next, and so you get them for free.
 ///
 /// Iterators are also composable, and it's possible to chain them together to
@@ -40,20 +42,24 @@ namespace sus::iter {
 ///
 /// An iterator has two required methods.
 ///
-/// `next()`, which when called, returns an `Option<Item>`. Calling next will
-/// return an `Option` containing the next `Item` as long as there are elements,
-/// and once they've all been exhausted, will return `None` to indicate that
-/// iteration is finished. Individual iterators may choose to resume iteration,
-/// and so calling next again may or may not eventually start returning an
-/// `Item` again at some point.
+/// * `auto next() -> `[`Option`]($sus::option::Option)`<Item>`
+/// 
+///   Returns an [`Option`]($sus::option::Option)
+///   containing the next `Item` as long as there are elements,
+///   and once they've all been exhausted, will return `None` to indicate that
+///   iteration is finished. Individual iterators may choose to resume
+///   iteration, and so calling next again may or may not eventually start
+///   returning an `Item` again at some point.
 ///
-/// `size_hint()` that returns a `sus::iter::SizeHint` containing a lower bound
-/// and optional upper bound on the number of elements left to be yielded by the
-/// iterator. An upper bound of `None` indicates either an unknown upper bound
-/// or a bound that is larger than `usize`. Rerturning `lower = 0` and `upper =
-/// None` is correct for any iterator, but providing a more accurate bound can
-/// benefit performance optiomizations. Returning an incorrect bound is
-/// technically possible but is a violation of the Iterator protocol.
+/// * `auto size_hint() const -> `[`SizeHint`]($sus::iter::SizeHint)
+/// 
+///   Returns a [`SizeHint`]($sus::iter::SizeHint) containing a lower bound
+///   and optional upper bound on the number of elements left to be yielded by
+///   the iterator. An upper bound of `None` indicates either an unknown upper
+///   bound or a bound that is larger than `usize`. Returning `lower = 0` and
+///   `upper = None` is correct for any iterator, but providing a more accurate
+///   bound can benefit performance optiomizations. Returning an incorrect bound
+///   is technically possible but is a violation of the `Iterator` protocol.
 template <class T, class Item = typename std::remove_cvref_t<T>::Item>
 concept Iterator =
     requires(std::remove_cvref_t<T>& t, const std::remove_cvref_t<T>& c) {
@@ -69,7 +75,7 @@ concept Iterator =
       { c.size_hint() } noexcept -> std::same_as<SizeHint>;
     };
 
-/// An `Iterator` able to yield elements from both ends.
+/// An [`Iterator`]($sus::iter::Iterator) able to yield elements from both ends.
 ///
 /// Something that implements `DoubleEndedIterator` has one extra capability
 /// over something that implements `Iterator`: the ability to also take Items
@@ -78,32 +84,45 @@ concept Iterator =
 /// It is important to note that both back and forth work on the same range, and
 /// do not cross: iteration is over when they meet in the middle.
 ///
-/// In a similar fashion to the `Iterator` protocol, once a
+/// In a similar fashion to the [`Iterator`]($sus::iter::Iterator) protocol,
+/// once a
 /// `DoubleEndedIterator` returns `None` from a `next_back()`, calling it again
 /// may or may not ever return `Some` again. `next()` and `next_back()` are
 /// interchangeable for this purpose.
+///
+/// # Required methods.
+///
+/// A `DoubleEndedIterator` has one required methods, in addition to those
+/// required by [`Iterator`]($sus::iter::Iterator).
+///
+/// * `auto next_back() -> Option<Item>` returns an `Option` containing the next
+/// `Item` from the back of the iterator as long as there are elements,
+/// and once they've all been exhausted, will return `None` to indicate that
+/// iteration is finished.
 template <class T, class Item = typename std::remove_cvref_t<T>::Item>
 concept DoubleEndedIterator =
     Iterator<T, Item> && requires(std::remove_cvref_t<T>& t) {
       { t.next_back() } noexcept -> std::same_as<::sus::option::Option<Item>>;
     };
 
-/// An iterator that knows its exact length.
+/// An [`Iterator`]($sus::iter::Iterator) that knows its exact length.
 ///
-/// Many Iterators don’t know how many times they will iterate, but some do. If
+/// Many [`Iterator`]($sus::iter::Iterator)s don’t know how many times they will
+/// iterate, but some do. If
 /// an iterator knows how many times it can iterate, providing access to that
 /// information can be useful. For example, if you want to iterate backwards, a
 /// good start is to know where the end is.
 ///
-/// When implementing an `ExactSizeIterator`, you must also implement
-/// `Iterator`.
+/// # Required methods.
 ///
-/// Then, to implement `ExactSizeIterator`, provide the `exact_size_hint()`
-/// method that returns the exact size of the iterator. The implementation of
-/// `Iterator::size_hint()` must also return the exact size of the iterator
-/// (usually by calling `exact_size_hint()`).
-//
-// TODO: Rename exact_size_hint() to len()?
+/// An `ExactSizeIterator` has one required methods, in addition to those
+/// required by [`Iterator`]($sus::iter::Iterator).
+///
+/// * `auto exact_size_hint() -> usize` returns the exact size of the iterator.
+/// The implementation of `Iterator::size_hint()` must also return the exact
+/// size of the iterator (usually by calling `exact_size_hint()`).
+///
+/// TODO: Rename `exact_size_hint` to `len`?
 template <class T, class Item = typename std::remove_cvref_t<T>::Item>
 concept ExactSizeIterator =
     Iterator<T, Item> && requires(const std::remove_cvref_t<T>& t) {
