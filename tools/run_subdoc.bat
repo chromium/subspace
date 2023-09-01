e27bcdd895652e3c366f9135d1e994241f7e6125
Dana Jansens <danakj@orodu.net>
2023-08-31 23:10:45 -0400

    Add a placeholder logo and make Subdoc use it as the favicon

--- tools/run_subdoc.bat
+++ tools/run_subdoc.bat
@@ -3,6 +3,8 @@ out\subdoc\subdoc -p out --out docs ^
     --exclude-file-pattern /third_party/ ^
     --css subdoc-test-style.css ^
     --copy-file subdoc/gen_tests/subdoc-test-style.css ^
+    --copy-file web/logo.png ^
+    --favicon "logo.png;image/png" ^
     --project-md sus/project.md ^
     --project-name Subspace ^
     --ignore-bad-code-links ^
