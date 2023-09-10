54b831a10429e09297098c69f77ddfcd634ecac2
Dana Jansens <danakj@chromium.org>
2023-09-10 17:28:26 -0400

    Remove use of FnBox in subdoc RunOptions
    Use Box<DynFn> instead.

--- subdoc/lib/run_options.h
+++ subdoc/lib/run_options.h
@@ -17,6 +17,7 @@
 #include <regex>
 
 #include "subdoc/llvm.h"
+#include "sus/boxed/box.h"
 #include "sus/fn/fn.h"
 #include "sus/option/option.h"
 #include "sus/prelude.h"
@@ -39,7 +40,8 @@ struct RunOptions {
     return sus::move(*this);
   }
   RunOptions set_on_tu_complete(
-      sus::fn::FnBox<void(clang::ASTContext&, clang::Preprocessor&)> fn) && {
+      sus::Box<sus::fn::DynFn<void(clang::ASTContext&, clang::Preprocessor&)>>
+          fn) && {
     on_tu_complete = sus::some(sus::move(fn));
     return sus::move(*this);
   }
@@ -54,7 +56,8 @@ struct RunOptions {
   ///
   /// Used for tests to observe the AST and test subdoc methods that act on
   /// things from the AST.
-  sus::Option<sus::fn::FnBox<void(clang::ASTContext&, clang::Preprocessor&)>>
+  sus::Option<
+      sus::Box<sus::fn::DynFn<void(clang::ASTContext&, clang::Preprocessor&)>>>
       on_tu_complete;
   /// The overview markdown which will be applied as the doc comment to the
   /// global namespace/project overview page. This is the raw markdown text, not
