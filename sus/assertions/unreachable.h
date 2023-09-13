063c6603216f4e0d4f324ee818db728e6acbc2cf
danakj <danakj@chromium.org>
2023-09-13 18:37:13 -0400

    Document unreachable and unreachable_unchecked

--- sus/assertions/unreachable.h
+++ sus/assertions/unreachable.h
@@ -17,17 +17,43 @@
 #include <source_location>
 
 #include "sus/assertions/panic.h"
-#include "sus/macros/inline.h"
 #include "sus/macros/builtin.h"
+#include "sus/macros/inline.h"
 #include "sus/marker/unsafe.h"
 
 namespace sus::assertions {
 
+/// Indicates to the developer that the location should not be reached, and
+/// terminates the program with a [`panic`]($sus::assertions::panic).
+///
+/// This is similar to [`std::unreachable!`](
+/// https://doc.rust-lang.org/stable/std/macro.unreachable.html) in Rust,
+/// and in the default build configuration will print an error message
+/// indicating the location where the panic occurred.
+///
+/// Note that this is *not* the same as [`std::unreachable`](
+/// https://en.cppreference.com/w/cpp/utility/unreachable) in C++ which is
+/// Undefined Behaviour if reached. It is closer to [`std::abort`](
+/// https://en.cppreference.com/w/cpp/utility/program/abort) except built on
+/// top of [`panic`]($sus::assertions::panic).
+/// The Subspace library matches the safer behaviour of Rust to avoid confusion
+/// and security bugs when working across languages. Use
+/// [`unreachable_unchecked`]($sus::assertions::unreachable_unchecked) to
+/// indicate to the compiler the code is not reachable.
 [[noreturn]] sus_always_inline void unreachable(
     const std::source_location location = std::source_location::current()) {
   panic(location);
 }
 
+/// Indicates to the compiler that the location will never be reached, allowing
+/// it to optimize code generation accordingly. If this function is actually
+/// reached, Undefined Behaviour will occur.
+///
+/// This is similar to [`std::unreachable`](
+/// https://en.cppreference.com/w/cpp/utility/unreachable).
+///
+/// # Safety
+/// This function must never actually be reached, or Undefined Behaviour occurs.
 [[noreturn]] sus_always_inline void unreachable_unchecked(
     ::sus::marker::UnsafeFnMarker) {
 #if __has_builtin(__builtin_unreachable)
