c7249cf584a37b37c20f60928636d0b2a1e976bb
Dana Jansens <danakj@chromium.org>
2023-09-24 02:19:48 -0400

    Drop the reverse and reserve_by functions
    Now that Reverse isn't terrible to use and demonstrate, we will
    just keep the thing that is also in Rust stdlib. It's easy to write
    a reversing lambda anyhow.

--- sus/cmp/reverse_unittest.cc
+++ sus/cmp/reverse_unittest.cc
@@ -20,23 +20,6 @@
 namespace {
 using namespace sus::cmp;
 
-TEST(Reverse, Example_ReverseFunction) {
-  auto v = sus::Vec<i32>(1, 2, 3, 4, 5, 6);
-  v.sort_by(sus::cmp::reverse<i32>);
-  sus::check(v == sus::Vec<i32>(6, 5, 4, 3, 2, 1));
-}
-
-TEST(Reverse, Example_ReverseByFunction) {
-  auto v = sus::Vec<i32>(1, 2, 3, 4, 5, 6);
-  v.sort_by(sus::cmp::reverse_by<i32>([](i32 a, i32 b) -> std::strong_ordering {
-    const bool a_small = a <= 3;
-    const bool b_small = b <= 3;
-    if (a_small != b_small) return a_small <=> b_small;
-    return a <=> b;
-  }));
-  sus::check(v == sus::Vec<i32>(3, 2, 1, 6, 5, 4));
-}
-
 TEST(Reverse, Example_ReverseKey) {
   using sus::cmp::Reverse;
 
