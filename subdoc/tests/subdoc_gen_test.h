3f37e171230cce4e81a23bbfaa3e1d1ae9621fb8
danakj <danakj@chromium.org>
2025-04-24 15:56:48 -0400

    Rebaseline subdoc tests to include the og:url tag

--- subdoc/tests/subdoc_gen_test.h
+++ subdoc/tests/subdoc_gen_test.h
@@ -54,6 +54,7 @@ class SubDocGenTest : public testing::Test {
     using subdoc::gen::FavIcon;
     using namespace std::string_literals;
     auto options = subdoc::gen::Options{
+        .project_url = sus::some("PROJECT_URL"),
         .version_text = sus::some("VERSION_STRING"s),
         .output_root =
             [&]() {
