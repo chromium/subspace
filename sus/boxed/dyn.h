dbfdd7cff890130b915cade773f0bf0206d7aa17
Christopher Di Bella <cjdb@google.com>
2025-05-23 08:58:06 -0700

    moves macros into their own headers (#469)
    This makes it possible to modularise Subspace, since some parts of the
    library are preprocessor-dependent. No change is necessary when building
    without modules. When building with modules, users will need to include
    `sus/<sublib>/macros.h` in order to get the relevant headers.
    
    ---------
    
    Co-authored-by: Dana Jansens <danakj@orodu.net>

--- sus/boxed/dyn.h
+++ sus/boxed/dyn.h
@@ -18,6 +18,7 @@
 #include <type_traits>
 
 #include "sus/boxed/boxed.h"  // namespace docs.
+#include "sus/boxed/macros.h"
 #include "sus/macros/lifetimebound.h"
 #include "sus/mem/forward.h"
 #include "sus/mem/move.h"
@@ -437,54 +438,3 @@ constexpr Dyn<DynC, ConcreteT> dyn(
 namespace sus {
 using sus::boxed::dyn;
 }
-
-/// Macro to help implement `DynC` for a concept `C`. The macro is placed in the
-/// body of the `DynC` class.
-///
-/// Here `DynC` is used as a placeholder name to refer to the virtual class
-/// that type-erases for the concept `C`. The type erasure class is typically
-/// named to match the concept, with a "Dyn" prefix. The type-aware subclass
-/// of the type erasure class is typically named to match the concept with a
-/// "Dyn" prefix and a "Typed" suffix.
-///
-/// The `Concept` parameter is the concept `C` for which types are being
-/// type-erased.
-///
-/// The `DynConcept` parameter is the name of the type-erasure
-/// class `DynC` which the macro is written within, and which has a pure virtual
-/// interface matching the concept's requirements.
-///
-/// The `DynConceptTyped`
-/// parameter is the type-aware subclass of `DynC` which contains the
-/// `sus_dyn_concept_typed` macro in its body, and the
-/// implementation of the virtual interface that forwards calls through to the
-/// concrete type.
-///
-/// See [`DynConcept`]($sus::boxed::DynConcept) for more on type erasure of
-/// concept-satisfying types, and [DynConcept examples](
-/// $sus::boxed::DynConcept#examples) for examples of using the macro.
-#define sus_dyn_concept(Concept, DynConcept, DynConceptTyped)  \
- public:                                                       \
-  template <class ConcreteT>                                   \
-  static constexpr bool SatisfiesConcept = Concept<ConcreteT>; \
-  template <class ConcreteT, class Store>                      \
-  using DynTyped = DynConceptTyped<ConcreteT, Store>;          \
-                                                               \
-  DynConcept() = default;                                      \
-  virtual ~DynConcept() = default;                             \
-  DynConcept(DynConcept&&) = delete;                           \
-  DynConcept& operator=(DynConcept&&) = delete
-
-/// Macro to help implement `DynCTyped` for a concept `C`. The macro is placed
-/// in the body of the `DynCTyped` class.
-///
-/// See the TODO: link [`sus_dyn_concept`] macro for more, and
-/// [DynConcept examples]($sus::boxed::DynConcept#examples) for examples
-/// of using the macro.
-#define sus_dyn_concept_typed(Concept, DynConcept, DynConceptTyped, VarName)  \
- public:                                                                      \
-  static_assert(Concept<T>);                                                  \
-  constexpr DynConceptTyped(Store&& c) : VarName(::sus::forward<Store>(c)) {} \
-                                                                              \
- private:                                                                     \
-  Store VarName;
---
+++ sus/mem/never_value_macros.h
@@ -0,0 +1,54 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+/// Mark a class field as never being a specific value, often a zero, after a
+/// constructor has run and before the destructor has completed. This allows
+/// querying if a class is constructed in a memory location, since the class is
+/// constructed iff the value of the field is not the never-value.
+///
+/// The named field can be compared to the `never_value` to determine if the
+/// object is constructed. The field must be set to the `destroy_value` just
+/// prior to destruction. The latter is meant to help the destructor be a no-op
+/// when the type is in a never-value state, if the never-value would be read in
+/// the destructor.
+///
+/// The macro includes `private:` which changes the class definition visibility
+/// to private.
+#define sus_class_never_value_field(unsafe_fn, T, field_name, never_value,     \
+                                    destroy_value)                             \
+ private:                                                                      \
+  static_assert(                                                               \
+      std::same_as<decltype(unsafe_fn), const ::sus::marker::UnsafeFnMarker>); \
+                                                                               \
+  template <class>                                                             \
+  friend struct ::sus::mem::__private::NeverValueAccess;                       \
+  template <class>                                                             \
+  friend struct ::sus::mem::__private::NeverValueChecker;                      \
+                                                                               \
+  _sus_pure constexpr bool _sus_Unsafe_NeverValueIsConstructed(                \
+      ::sus::marker::UnsafeFnMarker) const noexcept {                          \
+    static_assert(                                                             \
+        std::is_assignable_v<decltype(field_name)&, decltype(never_value)>,    \
+        "The `never_value` must be able to be assigned to the named field.");  \
+    return !(field_name == never_value);                                       \
+  }                                                                            \
+  constexpr void _sus_Unsafe_NeverValueSetDestroyValue(                        \
+      ::sus::marker::UnsafeFnMarker) noexcept {                                \
+    static_assert(::sus::cmp::Eq<decltype(field_name), decltype(never_value)>, \
+                  "The `never_value` must be comparable to the named field."); \
+    field_name = destroy_value;                                                \
+  }                                                                            \
+  static_assert(true)
